8f97f9966a31c7a8a010cd0934004f6717c3f577
murtazaagaz <murtaza.agaz100@gmail.com>
2016-05-17 06:38:31 -0700

    otp & no internet layout

---
+++ app/src/main/java/com/hackerkernel/blooddonar/activity/Verify_OTP_Activity.java
@@ -0,0 +1,15 @@
+package com.hackerkernel.blooddonar.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hackerkernel.blooddonar.R;
+
+public class Verify_OTP_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify__otp_);
+    }
+}
