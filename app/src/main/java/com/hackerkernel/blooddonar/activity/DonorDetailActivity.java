76778c0e9edbccd9adb0e2c43a40d3a1d6191e2f
murtazaagaz <murtaza.agaz100@gmail.com>
2016-05-25 05:52:24 -0700

    Completed detail donor layout

---
+++ app/src/main/java/com/hackerkernel/blooddonar/activity/DonorDetailActivity.java
@@ -0,0 +1,15 @@
+package com.hackerkernel.blooddonar.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hackerkernel.blooddonar.R;
+
+public class DonorDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_detail);
+    }
+}
