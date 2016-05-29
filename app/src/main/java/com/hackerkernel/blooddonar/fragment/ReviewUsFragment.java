e8d32bf0eb2af0494176dd04a563bdff9ad4ee1c
murtazaagaz <murtaza.agaz100@gmail.com>
2016-05-29 20:16:33 +0530

    Completed AlerDialog

--- app/src/main/java/com/hackerkernel/blooddonar/fragment/ReviewUsFragment.java
+++ app/src/main/java/com/hackerkernel/blooddonar/fragment/ReviewUsFragment.java
@@ -1,6 +1,7 @@
 package com.hackerkernel.blooddonar.fragment;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -9,17 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Gallery;
 import android.widget.Toast;
 
 import com.hackerkernel.blooddonar.R;
+
+import java.util.zip.Inflater;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ReviewUsFragment extends Fragment {
-    private String statusText;
-    private EditText statusEditText;
+    private String statusText, caption;
+    private EditText statusEditText, captionEditText;
+    private Button addPhotoBtn;
     @Bind(R.id.post_status_btn) Button statusButton;
+    @Bind(R.id.post_photo)Button postPhotoButton;
 
 
     public ReviewUsFragment() {
@@ -35,7 +41,7 @@ public class ReviewUsFragment extends Fragment {
         statusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openAlertDialoge();
+                openStatusAlertDialoge();
                 statusEditText.setText("");
             }
         });
@@ -47,12 +53,12 @@ public class ReviewUsFragment extends Fragment {
     }
 
 
-    private void openAlertDialoge(){
+    private void openStatusAlertDialoge(){
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         builder.setTitle("Post Status");
         View status = LayoutInflater.from(getActivity())
-                .inflate(R.layout.alert_dialoge_layout, null);
+                .inflate(R.layout.alert_status_dialoge, null);
         statusEditText = (EditText) status.findViewById(R.id.edit_status);
 
         builder.setView(status);
@@ -67,6 +73,41 @@ public class ReviewUsFragment extends Fragment {
         });
         AlertDialog dialog = builder.create();
         dialog.show();
+        postPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPhotoAlertDialog();
+            }
+        });
+
+    }
+
+    private void openPhotoAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Post Picture");
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.alert_image_dialoge,null);
+        captionEditText = (EditText) view.findViewById(R.id.edit_caption);
+        addPhotoBtn = (Button) view.findViewById(R.id.select_photo_btn);
+        addPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK);
+                i.setType("image/*");
+                startActivityForResult(i,100);
+            }
+        });
+        caption = captionEditText.getText().toString();
+        builder.setView(view);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
     }
 
 }
